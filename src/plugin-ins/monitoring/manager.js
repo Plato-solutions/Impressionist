0c97017b6f196b168eb9806743f3b58e76da5313
Rhonal Velasco <rhonal@platoanalytics.com>
2021-10-17 10:19:55 -0500

    feat - Add JSdoc comments.

--- src/plugin-ins/monitoring/manager.js
+++ src/plugin-ins/monitoring/manager.js
@@ -1,16 +1,33 @@
-
+/**
+ * Log on each of the subscribed monitoring tools.
+ */
 class MonitorManager {
     
+    /**
+     * Monitoring and logging tools.
+     */
     static #monitors = [];
 
+    /**
+     * Register a report.
+     * @param { object } report - Information to be logged in.
+     */
     static log(report) {
 
     }
 
+    /**
+     * Subscribe to a monitoring or logging tool.
+     * @param { object } logger - Monitoring or logging tool.
+     */
     static subscribe(logger) {
         
     }
 
+    /**
+     * Unsubscribe to a monitoring or logging tool.
+     * @param { object } logger - Monitoring or logging tool.
+     */
     static unsubscribe(logger) {
 
     }
