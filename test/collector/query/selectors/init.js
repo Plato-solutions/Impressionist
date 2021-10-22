b3be38f3815e35785dc82b654844372d7c4fa7b2
Rhonal Velasco <91996956+RhonalVelasco@users.noreply.github.com>
2021-10-22 11:39:02 -0500

    Add Init Selector for Query chains. (#10)
    * feat - Add Error unit tests.
    
    * feat - Add Init unit test for constructor initialization.
    
    * feat - Add Init implementation as Selector
    
    * refactor - Add construct handler for Proxy object.
    
    * feat - Add error for arguments.
    
    * feat - Add init as proxy function in the namespace
    
    * refactor - Remove extra unit tests.
    
    * refactor - Change function name and parameters name for a better code documentation.
    
    * feat - Add unit test for faulty function.
    
    * refactor - Add try/catch blocks.
    
    * refactor - Remove .only statements.

---
+++ test/collector/query/selectors/init.js
@@ -0,0 +1,124 @@
+import * as Impressionist from '../../../../src/index.js';
+import puppeteer  from 'puppeteer';
+import NanoServer from '../../../testing-server/server.js';
+import assert from 'assert';
+
+describe('Selector - Init', () => {
+    
+    const testingServer = new NanoServer();
+    const url = 'http://localhost:8081';
+
+    let browser;
+    let page;
+
+    before(async () => {
+        await testingServer.start();
+        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+        page = await browser.newPage();
+        await Impressionist.Process.setPageConfigurations(page, url);
+    });
+
+    describe('Execution Errors', () => {
+
+        it('Passing an undefined context', async () => {
+            
+            async function throwError() {    
+
+                return await page.evaluate(async () => {
+                    const selector = new Init((context) => context);
+
+                    return await selector.call(); // Undefined context;
+                });
+
+            }    
+            
+            await assert.rejects(throwError, {
+                name: 'Error',
+                message: /Init - The context is not an instance of Context class./
+            });
+
+        });
+
+        it('Passing a context not instance of Context', async () => {
+            
+            async function throwError() {    
+
+                return await page.evaluate(async () => {
+                    const selector = new Init((context) => context);
+
+                    return await selector.call({ context: 'fake' });
+                });
+
+            } 
+            
+            await assert.rejects(throwError, {
+                name: 'Error',
+                message: /Init - The context is not an instance of Context class./
+            });
+
+        });
+
+    });
+
+    describe('Initialization', () => {
+
+        it('Passing no function definition', async () => {
+
+            async function throwError() {    
+
+                return await page.evaluate(async () => {
+                    const selector = init(123);
+
+                    return await selector.call(new Context());
+                });
+
+            } 
+            
+            await assert.rejects(throwError, {
+                name: 'Error',
+                message: /Init - Constructor definition should be a function. Instead it received a number./
+            });
+
+        });
+
+        it('Passing a faulty function as definition', async () => {
+
+            async function throwError() {    
+
+                return await page.evaluate(async () => {
+                    const selector = init(() => { throw new Error('DOM element does not exists.') });
+
+                    return await selector.call(new Context());
+                });
+
+            } 
+            
+            await assert.rejects(throwError, {
+                name: 'Error',
+                message: /InitialFunction - DOM element does not exists./
+            });
+
+        });
+        
+        it('Change h1 innerText before execution of the Query', async () => {
+            const result = await page.evaluate(async () => { 
+    
+                const data = new Collection({
+                    name: css('h1').property('innerText').single().init(() => { document.querySelector('h1').innerText = 'Plato Plugin Modified' }) // init at the end of the query chain assure the last execution.
+                });
+                    
+                const context = new Context();
+                return await data.call(context);
+            });
+        
+            assert.deepStrictEqual(result, { name: 'Plato Plugin Modified' });
+        });
+    });
+
+    after(async () => {
+        await page.close();
+        await browser.close();
+        await testingServer.stop();
+    });
+
+});
