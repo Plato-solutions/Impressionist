4d5a2bd79141bfb2b024869d4376f53bb8da32f4
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-08 13:58:10 -0500

    WA-1421: feat - Add initialize and close method implementation and tests.

---
+++ test/src/browserControllers/puppeteer/puppeteerController.js
@@ -0,0 +1,55 @@
+/*
+ Copyright 2021 Plato Solutions, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import assert from "assert";
+import NanoServer from '../../../testing-server/server.js';
+import PuppeteerController from '../../../../src/browserControllers/puppeteer/puppeteerController.js';
+import { isRegExp } from "util/types";
+
+describe.only('PuppeteerController Class', () => {
+
+    const testingServer = new NanoServer();
+    const url = 'http://localhost:8081/';
+
+    before(async () => {
+        await testingServer.start();
+    });
+
+    describe('Initialize Puppeteer', () => {
+
+        it('Create a configured browser and page instances', async () => {
+            const result = PuppeteerController.initialize();
+            await assert.doesNotReject(result);
+            await PuppeteerController.close();
+        });
+
+    });
+
+    describe('Close Puppeteer conections', () => {
+        
+        it('Close browser and page', async () => {
+            await PuppeteerController.initialize();
+            const result = PuppeteerController.close();
+            await assert.doesNotReject(result);
+        });
+
+    });
+    
+    after(async () => {
+        await testingServer.stop();
+    });
+
+});
