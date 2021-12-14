1fc6607fecda1091ef927c257e20a14f96eec977
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 05:13:01 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/factory/option.js
+++ test/collector/factory/option.js
@@ -16,52 +16,36 @@
 
 
 import assert from "assert";
-import * as Impressionist from '../../../src/index.js';
+import { Context } from "../../../lib/index.js";
+import Impressionist from '../../../src/process.js';
 import NanoServer from '../../testing-server/server.js';
-import puppeteer from 'puppeteer';
 
 describe('OptionCollectorFactory Class', () => {
 
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
 
-    let browser;
-    let page;
-
     before(async () => {
         await testingServer.start();
-        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        page = await browser.newPage();
-        await Impressionist.Process.setPageConfigurations(page, url);
     });
 
     it('Return a list of Elements', async () => {
-        const result = await page.evaluate(async () => {
-
-            const data = ( function () {
-
-                const css = SelectorDirectory.get('css');
-    
-                return new OptionCollectorFactory(
-                    {
-                        edition: '{#option-1}',
-                        support: '{#option-2}'
-                    }
-                );
         
-            } )();
-
-            const context = new Context();
-            const values = await data.call(context);
-
-            let result = [];
-
-            for await(let value of values[0]) {
-                result.push(value);
-            }
-
-            return result;
-
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
+                const values = await new OptionCollectorFactory({
+                    edition: '{#option-1}',
+                    support: '{#option-2}'    
+                }).call(new Context());
+            
+                let result = [];
+    
+                for await(let value of values[0]) {
+                    result.push(value);
+                }
+    
+                return result;
+            });
         });
 
         assert.deepStrictEqual(result, [
@@ -93,72 +77,60 @@ describe('OptionCollectorFactory Class', () => {
     });
 
     it('Return a list of Elements. Does not print non-existing options.', async () => {
-      const result = await page.evaluate(async () => {
+        
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
+                const values = await new OptionCollectorFactory({
+                    edition: '{#option-1}',
+                    support: '{#option-2}',
+                    additional: '{#fake-option}'
+                }).call(new Context());
+    
+                let result = [];
+    
+                for await(let value of values[0]) {
+                    result.push(value);
+                }
+    
+                return result;
+            });
+        });
 
-          const data = ( function () {
-
-              const css = SelectorDirectory.get('css');
-  
-              return new OptionCollectorFactory(
-                  {
-                      edition: '{#option-1}',
-                      support: '{#option-2}',
-                      additional: '{#fake-option}'
-                  }
-              );
-      
-          } )();
-
-          const context = new Context();
-          const values = await data.call(context);
-
-          let result = [];
-
-          for await(let value of values[0]) {
-              result.push(value);
-          }
-
-          return result;
-
-      });
-
-      assert.deepStrictEqual(result, [
-        [
-          { value: null, additional: null },
-          { value: '40', support: 'val-40' },
-          { value: '10', edition: 'val-10' }
-        ],
-        [
-          { value: null, additional: null },
-          { value: '50', support: 'val-50' },
-          { value: '10', edition: 'val-10' }
-        ],
-        [
-          { value: null, additional: null },
-          { value: '40', support: 'val-40' },
-          { value: '20', edition: 'val-20' }
-        ],
-        [
-          { value: null, additional: null },
-          { value: '50', support: 'val-50' },
-          { value: '20', edition: 'val-20' }
-        ],
-        [
-          { value: null, additional: null },
-          { value: '40', support: 'val-40' },
-          { value: '30', edition: 'val-30' }
-        ],
-        [
-          { value: null, additional: null },
-          { value: '50', support: 'val-50' },
-          { value: '30', edition: 'val-30' }
-        ]
-      ]);
-  });
+        assert.deepStrictEqual(result, [
+            [
+                { value: null, additional: null },
+                { value: '40', support: 'val-40' },
+                { value: '10', edition: 'val-10' }
+            ],
+            [
+                { value: null, additional: null },
+                { value: '50', support: 'val-50' },
+                { value: '10', edition: 'val-10' }
+            ],
+            [
+                { value: null, additional: null },
+                { value: '40', support: 'val-40' },
+                { value: '20', edition: 'val-20' }
+            ],
+            [
+                { value: null, additional: null },
+                { value: '50', support: 'val-50' },
+                { value: '20', edition: 'val-20' }
+            ],
+            [
+                { value: null, additional: null },
+                { value: '40', support: 'val-40' },
+                { value: '30', edition: 'val-30' }
+            ],
+            [
+                { value: null, additional: null },
+                { value: '50', support: 'val-50' },
+                { value: '30', edition: 'val-30' }
+            ]
+        ]);
+    });
 
     after(async () => {
-        await page.close();
-        await browser.close();
         await testingServer.stop();
     });
 
