06b96d179b3847958bafe31e5cd83b53ac4af401
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 06:42:47 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/query/selectors/collector.js
+++ test/collector/query/selectors/collector.js
@@ -18,7 +18,7 @@ import Impressionist from '../../../../src/process.js';
 import NanoServer from '../../../testing-server/server.js';
 import assert from 'assert';
 
-describe.only('Selector - Collector', async () => {
+describe('Selector - Collector', async () => {
     
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
