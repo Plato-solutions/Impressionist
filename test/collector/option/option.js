b8f719b09252284e286f18e1ef9232ae1e177358
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 05:42:21 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/option/option.js
+++ test/collector/option/option.js
@@ -15,41 +15,33 @@
  */
 
 import assert from "assert";
-import * as Impressionist from '../../../src/index.js';
+import Impressionist from '../../../src/process.js';
 import NanoServer from '../../testing-server/server.js';
-import puppeteer from 'puppeteer';
 
 describe('Option Class', () => {
 
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
 
-    let browser;
-    let page;
-
     before(async () => {
         await testingServer.start();
-        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        page = await browser.newPage();
-        await Impressionist.Process.setPageConfigurations(page, url);
     });
   
     it('Handle Options for a Select Element', async () => {
             
-        const result = await page.evaluate(async () => {
-            const selectElement = document.querySelector('#option-1');
-
-            const optionsIterable = new Option('edition', selectElement);
-            
-            const options = optionsIterable.call();
-
-            let result = [];
-
-            for await (let option of options) {
-                result.push(option);
-            }
-
-            return result;
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
+                const selectElement = document.querySelector('#option-1');
+                const optionsIterable = await new Option('edition', selectElement).call();
+                
+                let result = [];
+    
+                for await (let option of optionsIterable) {
+                    result.push(option);
+                }
+    
+                return result;
+            });
         });
 
         assert.deepStrictEqual(result, [
@@ -71,20 +63,19 @@ describe('Option Class', () => {
 
     it('Handle Options with CheckboxStrategy', async () => {
         
-        const result = await page.evaluate(async () => {
-            const checkboxElement = Array.from(document.querySelectorAll('#div-1 > div'));
-
-            const optionsIterable = new Option('edition', checkboxElement);
-            
-            const options = optionsIterable.call();
-
-            let result = [];
-
-            for await (let option of options) {
-                result.push(option);
-            }
-
-            return result;
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
+                const checkboxElement = Array.from(document.querySelectorAll('#div-1 > div'));
+                const optionsIterable = await new Option('edition', checkboxElement).call();
+                
+                let result = [];
+    
+                for await (let option of optionsIterable) {
+                    result.push(option);
+                }
+    
+                return result;
+            });
         });
 
         assert.deepStrictEqual(result, [
@@ -106,20 +97,19 @@ describe('Option Class', () => {
 
     it('Handle Options with ToogleStrategy', async () => {
         
-        const result = await page.evaluate(async () => {
-            const toogleElement = document.querySelector('#toogle');
-
-            const optionsIterable = new Option('installation', toogleElement);
-            
-            const options = optionsIterable.call();
-
-            let result = [];
-
-            for await (let option of options) {
-                result.push(option);
-            }
-
-            return result;
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
+                const toogleElement = document.querySelector('#toogle');
+                const optionsIterable = await new Option('installation', toogleElement).call();
+                
+                let result = [];
+    
+                for await (let option of optionsIterable) {
+                    result.push(option);
+                }
+    
+                return result;
+            });
         });
 
         assert.deepStrictEqual(result, [
@@ -136,8 +126,6 @@ describe('Option Class', () => {
     });
 
     after(async () => {
-        await page.close();
-        await browser.close();
         await testingServer.stop();
     });
 
