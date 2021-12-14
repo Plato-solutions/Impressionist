2cd7819059f474e6d3e1153b169b7a6cd0a352b4
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 06:28:35 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/query/interpreter/strategy/manager.js
+++ test/collector/query/interpreter/strategy/manager.js
@@ -14,7 +14,6 @@
  limitations under the License.
  */
 
-import puppeteer  from 'puppeteer';
 import NanoServer from '../../../../testing-server/server.js';
 import Impressionist from '../../../../../src/process.js'
 import assert from 'assert';
@@ -24,39 +23,31 @@ describe('Selector Interpreters - Manager', () => {
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
 
-    let browser;
-    let page;
-
     before(async () => {
         await testingServer.start();
-        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        page = await browser.newPage();
-        await Impressionist.setPageConfigurations(page, url);
     });
 
     describe('get method', () => {
 
         it('{h1} gets InterpreterElementStrategy', async () => {
                 
-            const result = await page.evaluate(async () => { 
-        
-                const interpreter = InterpreterStrategyManager.lookUp('{h1}');
-
-                return interpreter === InterpreterElementStrategy ? true : false;
-    
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => { 
+                    const interpreter = InterpreterStrategyManager.lookUp('{h1}');
+                    return interpreter === InterpreterElementStrategy;
+                });
             });
-        
+            
             assert.strictEqual(result, true);
         });
 
         it('h1 gets InterpreterInnerTextStrategy', async () => {
                 
-            const result = await page.evaluate(async () => { 
-        
-                const interpreter = InterpreterStrategyManager.lookUp('h1');
-
-                return interpreter === InterpreterInnerTextStrategy ? true : false;
-    
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => { 
+                    const interpreter = InterpreterStrategyManager.lookUp('h1');
+                    return interpreter === InterpreterInnerTextStrategy;
+                });
             });
         
             assert.strictEqual(result, true);
@@ -64,12 +55,11 @@ describe('Selector Interpreters - Manager', () => {
 
         it('h1{outerHTML} gets InterpreterPropertyStrategy', async () => {
                 
-            const result = await page.evaluate(async () => { 
-        
-                const interpreter = InterpreterStrategyManager.lookUp('h1{outerHTML}');
-                
-                return interpreter === InterpreterPropertyStrategy ? true : false;
-    
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => { 
+                    const interpreter = InterpreterStrategyManager.lookUp('h1{outerHTML}');
+                    return interpreter === InterpreterPropertyStrategy;
+                });
             });
         
             assert.strictEqual(result, true);
@@ -78,8 +68,6 @@ describe('Selector Interpreters - Manager', () => {
     });
 
     after(async () => {
-        await page.close();
-        await browser.close();
         await testingServer.stop();
     });
 
