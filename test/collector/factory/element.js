0e71b1b9ab8ad826793c60de944091fec83a8f75
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 05:03:53 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/factory/element.js
+++ test/collector/factory/element.js
@@ -16,82 +16,61 @@
 
 
 import assert from "assert";
-import * as Impressionist from '../../../src/index.js';
+import { Context } from "../../../lib/index.js";
+import Impressionist from '../../../src/process.js';
 import NanoServer from '../../testing-server/server.js';
-import puppeteer from 'puppeteer';
 
 describe('ElementCollectorFactory Class', () => {
 
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
 
-    let browser;
-    let page;
-
     before(async () => {
         await testingServer.start();
-        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        page = await browser.newPage();
-        await Impressionist.Process.setPageConfigurations(page, url);
     });
 
     it('Return a list of Elements', async () => {
-        const result = await page.evaluate(async () => {
-
-            const data = ( function () {
-
-                const css = SelectorDirectory.get('css');
-    
-                return new ElementCollectorFactory(css('#reviews > ul > li').all());
         
-            } )();
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
 
-            const context = new Context();
-            const result = await data.call(context);
-            
-            let values = [];
-
-            for await(let el of result[0]) {
-                values.push(el);
-            }
-
-            return values.length;
-
+                const result = await new ElementCollectorFactory(
+                    css('#reviews > ul > li').all()
+                ).call(new Context());
+    
+                let values = [];
+    
+                for await(let el of result[0]) {
+                    values.push(el);
+                }
+    
+                return values.length;
+            });
         });
 
         assert.strictEqual(result, 2);
     });
 
     it('Return an empty array because non-existing DOM elements', async () => {
-        const result = await page.evaluate(async () => {
-
-            const data = ( function () {
-
-                const css = SelectorDirectory.get('css');
-    
-                return new ElementCollectorFactory('{#reviews > ul > la}*');
         
-            } )();
-
-            const context = new Context();
-            const result = await data.call(context);
-            
-            let values = [];
-
-            for await(let el of result[0]) {
-                values.push(el);
-            }
-
-            return values.length;
-
+        const result = await Impressionist.execute(url, async(browser, page) => {
+            return await page.evaluate(async () => {
+                const result = await new ElementCollectorFactory('{#reviews > ul > la}*').call(new Context());
+                
+                let values = [];
+    
+                for await(let el of result[0]) {
+                    values.push(el);
+                }
+    
+                return values.length;
+            });
         });
 
         assert.strictEqual(result, 0);
     });
 
     after(async () => {
-        await page.close();
-        await browser.close();
         await testingServer.stop();
     });
 
