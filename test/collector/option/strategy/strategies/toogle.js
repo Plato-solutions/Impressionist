2018e58ec055eee79486d2807e92393336dd25a7
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 05:31:30 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/option/strategy/strategies/toogle.js
+++ test/collector/option/strategy/strategies/toogle.js
@@ -16,49 +16,39 @@
 
 
 import assert from "assert";
-import * as Impressionist from '../../../../../src/index.js';
+import Impressionist from '../../../../../src/process.js';
 import NanoServer from '../../../../testing-server/server.js';
-import puppeteer from 'puppeteer';
 
 describe('OptionStrategy - ToogleStrategy class', () => {
 
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
 
-    let browser;
-    let page;
-
     before(async () => {
         await testingServer.start();
-        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        page = await browser.newPage();
-        await Impressionist.Process.setPageConfigurations(page, url);
     });
   
     describe('match method', () => {
 
         it('match method returns true', async () => {
     
-            const result = await page.evaluate(async () => {
-                
-                const selectElement = document.querySelector('#toogle');
-                
-                return await ToogleStrategy.match(selectElement);
-                
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => {
+                    const selectElement = document.querySelector('#toogle');
+                    return await ToogleStrategy.match(selectElement);
+                });
             });
     
             assert.strictEqual(result, true);
-            
         });
     
         it('match method returns false', async () => {
     
-            const result = await page.evaluate(async () => {
-                
-                const selectElement = Array.from(document.querySelectorAll('div'));
-                
-                return await ToogleStrategy.match(selectElement);
-                
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => {
+                    const selectElement = Array.from(document.querySelectorAll('div'));
+                    return await ToogleStrategy.match(selectElement);
+                });
             });
     
             assert.strictEqual(result, false);
@@ -71,10 +61,11 @@ describe('OptionStrategy - ToogleStrategy class', () => {
 
         it('Get options from a checkbox element', async () => {
             
-            const result = await page.evaluate(async () => {
-                const selectElement = document.querySelector('#toogle');
-
-                return await ToogleStrategy.getOptions('installation', selectElement);
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => {
+                    const selectElement = document.querySelector('#toogle');
+                    return await ToogleStrategy.getOptions('installation', selectElement);
+                });
             });
 
             assert.deepStrictEqual(result, [
@@ -95,26 +86,27 @@ describe('OptionStrategy - ToogleStrategy class', () => {
     describe('setOption method', () => {
 
         it('Select an input element', async () => {
-            const result = await page.evaluate(async () => {
-                const inputElement = document.querySelector('#toogle > input');
 
-                // inputElement.click(); // If the input is checked by default;
-
-                await ToogleStrategy.setOption(inputElement, { click: inputElement, selection: true });
-
-                return inputElement.checked;
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => {
+                    const inputElement = document.querySelector('#toogle > input');
+                    // inputElement.click(); // If the input is checked by default;
+                    await ToogleStrategy.setOption(inputElement, { click: inputElement, selection: true });
+                    return inputElement.checked;
+                });
             });
 
             assert.strictEqual(result, true);
         });
 
         it('Unselect an input element', async () => {
-            const result = await page.evaluate(async () => {
-                const inputElement = document.querySelector('#toogle > input');
 
-                await ToogleStrategy.setOption(inputElement, { click: inputElement, selection: false });
-
-                return inputElement.checked;
+            const result = await Impressionist.execute(url, async(browser, page) => {
+                return await page.evaluate(async () => {
+                    const inputElement = document.querySelector('#toogle > input');
+                    await ToogleStrategy.setOption(inputElement, { click: inputElement, selection: false });
+                    return inputElement.checked;
+                });
             });
 
             assert.strictEqual(result, false);
@@ -123,8 +115,6 @@ describe('OptionStrategy - ToogleStrategy class', () => {
     });
 
     after(async () => {
-        await page.close();
-        await browser.close();
         await testingServer.stop();
     });
 
