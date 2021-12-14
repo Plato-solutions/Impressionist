d1ddefbe6cf7cb07369c494a6f974797721a20e0
Rhonal Velasco <rhonal@platoanalytics.com>
2021-12-14 07:28:06 -0500

    WA-1430: refactor - Use Impressionist in every unit tests.

--- test/collector/query/selectors/require.js
+++ test/collector/query/selectors/require.js
@@ -14,8 +14,7 @@
  limitations under the License.
  */
 
-import * as Impressionist from '../../../../src/index.js';
-import puppeteer  from 'puppeteer';
+import Impressionist from '../../../../src/process.js';
 import NanoServer from '../../../testing-server/server.js';
 import assert from 'assert';
 
@@ -24,47 +23,20 @@ describe('Selector - Require', () => {
     const testingServer = new NanoServer();
     const url = 'http://localhost:8081';
 
-    let browser;
-    let page;
-
     before(async () => {
         await testingServer.start();
-        browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        page = await browser.newPage();
-        await Impressionist.Process.setPageConfigurations(page, url);
     });
 
     describe('Errors', () => {
 
-        it('Passing an undefined context', async () => {
-            
-            async function missingThrowError() {    
-
-                return await page.evaluate(async () => {
-                    const selector = new Require();
-
-                    return await selector.call(); // Undefined context;
-                });
-
-            }    
-            
-            await assert.rejects(missingThrowError, {
-                name: 'Error',
-                message: /Require - The context is not an instance of Context class./
-            });
-
-        });
-
         it('Passing a context not instance of Context', async () => {
             
             async function missingThrowError() {    
-
-                return await page.evaluate(async () => {
-                    const selector = new Require();
-
-                    return await selector.call({ context: 'fake' });
+                return await Impressionist.execute(url, async(browser, page) => {
+                    return await page.evaluate(async () => {
+                        return await new Require().call({ context: 'fake' });
+                    });
                 });
-
             } 
             
             await assert.rejects(missingThrowError, {
@@ -78,8 +50,6 @@ describe('Selector - Require', () => {
 
 
     after(async () => {
-        await page.close();
-        await browser.close();
         await testingServer.stop();
     });
 
